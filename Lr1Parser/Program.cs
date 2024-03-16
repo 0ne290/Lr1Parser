7fda1ba7bd9e7965f3a973760e293ebcd03c40b2
One290 <lenya.dorovskoy@mail.ru>
2024-03-16 17:18:01 +0800

    Init

---
+++ Lr1Parser/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Lr1Parser;
+
+internal static class Program
+{
+    private static int Main()
+    {
+        /*try
+        {
+            // Work
+
+            Console.Write("Нажмите любую клавишу для завершения программы...");
+            Console.ReadKey();
+
+            return 0;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            Console.Write("\nНажмите любую клавишу для завершения программы...");
+            Console.ReadKey();
+
+            return 1;
+        }*/
+		
+        // Work
+
+        return 0;
+    }
+}
