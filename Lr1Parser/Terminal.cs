8d742e30c2878a43195126535cb129776cfe0a13
Алексей <97665299+0ne290@users.noreply.github.com>
2024-03-18 21:49:33 +0800

    Update Terminal.cs

--- Lr1Parser/Terminal.cs
+++ Lr1Parser/Terminal.cs
@@ -4,18 +4,25 @@ public class Terminal : IGrammarToken
 {
     private Terminal(string value) => Value = value;
 
-    public static void AddTerminalByValue(string value)
+    public static bool AddTerminalByValue(string value)
     {
         if (!Terminals.Exists(t => t.Value == value))
+        {
             Terminals.Add(new Terminal(value));
+            return true;
+        }
+
+        return false;
     }
 
-    public static Terminal? GetTerminalByValue(string value) =>
-        Terminals.Find(t => t.Value == value);
+    public static Terminal GetTerminalByValue(string value) =>
+        Terminals.Find(t => t.Value == value) ?? Empty;
+
+    public bool IsEmpty() => this == Empty;
     
     public string Value { get; set; }
 
-    public static Terminal Default { get; } = new(string.Empty);
+    public static Terminal Empty { get; } = new(string.Empty);
 
     private static readonly List<Terminal> Terminals = new();
 }
