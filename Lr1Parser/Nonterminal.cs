02de7a4b092256c556e015394e85575c020fce79
Алексей <97665299+0ne290@users.noreply.github.com>
2024-03-18 21:52:44 +0800

    Update Nonterminal.cs

--- Lr1Parser/Nonterminal.cs
+++ Lr1Parser/Nonterminal.cs
@@ -2,5 +2,27 @@ namespace Lr1Parser;
 
 public class Nonterminal : IGrammarToken
 {
-    public string Value { get; set; } = string.Empty;
+    private Nonterminal(string value) => Value = value;
+
+    public static bool AddNonterminalByValue(string value)
+    {
+        if (!Nonterminals.Exists(n => n.Value == value))
+        {
+            Nonterminals.Add(new Nonterminal(value));
+            return true;
+        }
+
+        return false;
+    }
+
+    public static Nonterminal GetNonterminalByValue(string value) =>
+        Nonterminals.Find(n => n.Value == value) ?? Empty;
+
+    public bool IsEmpty() => this == Empty;
+    
+    public string Value { get; set; }
+
+    public static Nonterminal Empty { get; } = new(string.Empty);
+
+    private static readonly List<Nonterminal> Nonterminals = new();
 }
